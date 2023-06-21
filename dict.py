9d76a7bfa3cda0801fc638e8c87bd1c3c67fd317
Masterxue <18600012580@163.com>
2023-06-21 22:14:47 +0800

    a

--- dict.py
+++ dict.py
@@ -6,7 +6,7 @@ time_start = time.time()
 df.fillna('Nan',inplace=True)
 dictation={}
 keys = ["phonetic","definition","translation","pos","collins","oxford","tag","bnc","frq","exchange","detail","audio"]
-translation = {"phonetic":"单词名称", "definition":"单词释义（英文）", "translation":"单词释义（中文）", "pos":"词语位置", "collins":"柯林斯星级", "oxford":"是否是牛津三千核心词汇", "tag":"字符串标签", "bnc":"英国国家语料库词频顺序", "frq":"当代语料库词频顺序", "exchange":"时态复数等变换", "detail":"json 扩展信息", "audio":"读音音频 url"}
+translation = {"phonetic":"音标", "definition":"单词释义（英文）", "translation":"单词释义（中文）", "pos":"词语位置", "collins":"柯林斯星级", "oxford":"是否是牛津三千核心词汇", "tag":"字符串标签", "bnc":"英国国家语料库词频顺序", "frq":"当代语料库词频顺序", "exchange":"时态复数等变换", "detail":"json 扩展信息", "audio":"读音音频 url"}
 for values in df.values:
     content={}
     for key_i in range (11):
