3a0c5d4f2c22b80e64e09ac206d0c6c8625f4b5c
Masterxue <18600012580@163.com>
2023-06-21 21:35:32 +0800

    建立字典并直接查找，缺乏数据处理

--- dict.py
+++ dict.py
@@ -4,5 +4,14 @@ df=pd.read_csv("ecdict.mini.csv")
 dictation={}
 for values in df.values:
     dictation[values[0]]=values[1:]
-print(dictation['by pass'])
+print(dictation)
+word=str(input())
+dic_word=dictation.get(word)
+if dic_word.any == None:
+    print('error')
+else:
+    for i in range(len(dic_word)):
+        if dic_word[i] != 'nan':
+            print('%s:%s'%(df.keys()[i+1],dic_word[i]))
+
 
