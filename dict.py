1cd5043e4b74e3e364da963b8973e5a6d536489b
Haoying Zhang <sicilyzhang409@gmail.com>
2023-06-21 15:21:37 +0200

    construction by dictionary structure

--- dict.py
+++ dict.py
@@ -1,8 +1,17 @@
 # Write code here
 import pandas as pd
+import time
 df=pd.read_csv("ecdict.mini.csv")
+time_start = time.time()
 dictation={}
+keys = ["phonetic","definition","translation","pos","collins","oxford","tag","bnc","frq","exchange","detail","audio"]
 for values in df.values:
-    dictation[values[0]]=values[1:]
+    content={}
+    for key_i in range (11):
+        key = keys[key_i]
+        content[key] = values[key_i+1]
+    dictation[values[0]] = content
+time_end = time.time()
+print("Dictionary construction delay: ",time_end-time_start,"s")
 print(dictation['by pass'])
 
