ad1e885cab2f47d3004db202aaf4e0e11a6093f8
Masterxue <18600012580@163.com>
2023-06-21 21:03:42 +0800

    建立字典并直接查找，缺乏数据处理

--- dict.py
+++ dict.py
@@ -1,4 +1,8 @@
 # Write code here
 import pandas as pd
 df=pd.read_csv("ecdict.mini.csv")
-print(df[["word","translation"]])
+dictation={}
+for values in df.values:
+    dictation[values[0]]=values[1:]
+print(dictation['by pass'])
+
